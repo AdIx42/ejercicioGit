0470caf8ffd106058643cd2f71233d8f44dad725
AdIx42 <74982846+AdIx42@users.noreply.github.com>
2024-10-30 11:28:42 -0600

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM openjdk:17-jdk-alpine
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+FROM node:10-alpine
+
+WORKDIR /home
+RUN npm config set strict-ssl false
+RUN npm install
+RUN npm install express --save
+
+
+COPY app.js .
+CMD [ "node", "app.js" ]
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = 8000;
+app.listen(port,()=> {
+console.log('listen port 8000');
+})
+
+app.get('/hello_world', (req,res)=>{
+res.send('Hello World');
+})
+
+app.get('/health', (req,res)=>{
+res.send('Ok');
+res.status(200);
+console.log('Probe enviado a /health');
+})
